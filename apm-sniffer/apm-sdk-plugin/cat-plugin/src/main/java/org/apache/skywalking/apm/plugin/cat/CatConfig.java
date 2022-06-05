543800fc9add7dab7ca086e4256c344738a249f5
dlorgine <344649710@qq.com>
2022-06-05 17:53:58 +0800

    1

---
+++ apm-sniffer/apm-sdk-plugin/cat-plugin/src/main/java/org/apache/skywalking/apm/plugin/cat/CatConfig.java
@@ -0,0 +1,8 @@
+package org.apache.skywalking.apm.plugin.cat;
+
+public class CatConfig {
+    final static String keyIps="cat.m_server.m_ips";
+    final static String keyTcpPort="cat.m_server.m_tcpPort";
+    final static String keyHttpPort="cat.m_server.m_httpPort";
+    final static String keyCatEnabled="cat.m_enabled";
+}
--- apm-sniffer/apm-sdk-plugin/cat-plugin/src/main/java/org/apache/skywalking/apm/plugin/cat/SpringInterceptor.java
+++ apm-sniffer/apm-sdk-plugin/cat-plugin/src/main/java/org/apache/skywalking/apm/plugin/cat/SpringInterceptor.java
@@ -1,5 +1,12 @@
 package org.apache.skywalking.apm.plugin.cat;
 
+import static org.apache.skywalking.apm.plugin.cat.CatConfig.keyCatEnabled;
+import static org.apache.skywalking.apm.plugin.cat.CatConfig.keyHttpPort;
+import static org.apache.skywalking.apm.plugin.cat.CatConfig.keyIps;
+import static org.apache.skywalking.apm.plugin.cat.CatConfig.keyTcpPort;
+
+import com.dianping.cat.Cat;
+import java.io.File;
 import java.lang.reflect.Method;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
@@ -23,18 +30,51 @@ public class SpringInterceptor implements InstanceMethodsAroundInterceptor {
     public Object afterMethod(
             EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes, Object ret
     ) throws Throwable {
-        ConfigurableEnvironment environment=(ConfigurableEnvironment)ret;
-        String domainName=environment.getProperty("app.name", environment.getProperty("spring.application.name"));
-        String catIps=environment.getProperty("cat.server.ips");
-        if(StringUtil.isNotEmpty(domainName)){
-          System.setProperty("spring.application.name", domainName);
+        ConfigurableEnvironment environment = (ConfigurableEnvironment) ret;
+        String domainName = environment.getProperty("cat.app.name", environment.getProperty("spring.application.name"));
+        System.setProperty("spring.application.name", domainName);
+        String catIps = environment.getProperty(keyIps);
+        String tcpPort = environment.getProperty(keyTcpPort, "2280");
+        String httpPort = environment.getProperty(keyHttpPort, "8080");
+
+        if ("true".equalsIgnoreCase(System.getProperty(CatConfig.keyCatEnabled, "true"))
+                && StringUtil.isNotEmpty(catIps)) {
+            System.out.println("Trying to initialize CAT with " + keyCatEnabled);
+            initialize(domainName, tcpPort, httpPort, catIps);
+        } else {
+            System.out.println("CAT is DISABLED explicitly. use ${cat.enabled} to turn on.");
         }
-        if(StringUtil.isNotEmpty(catIps)){
-          System.setProperty("cat.server.ips",catIps);
-        }
-        System.setProperty("cat.port",environment.getProperty("cat.port","2280"));
-        System.setProperty("cat.http.port",environment.getProperty("cat.http.port","8080"));
+
         return ret;
+    }
+
+
+    private void initialize(String domain, String tcpPort, String httpPort, String catIps) {
+
+        if (StringUtil.isNotEmpty(catIps)) {
+            // bring CAT up
+            String userHome = System.getProperty("user.home");
+            File catHome = null;
+            try {
+                catHome = new File(userHome, ".cat");
+                catHome.mkdirs();
+                System.setProperty("CAT_HOME", catHome.getAbsolutePath());
+            } catch (Throwable e) {
+                try {
+                    catHome = new File("/tmp/.cat");
+                    catHome.mkdirs();
+                    System.setProperty("CAT_HOME", catHome.getAbsolutePath());
+                } catch (Throwable ee) {}
+            }
+
+
+            Cat.getBootstrap()
+                    .initializeByDomain(domain,
+                            Integer.parseInt(tcpPort),
+                            Integer.parseInt(httpPort),
+                            catIps.trim().split(",")
+                    );
+        }
     }
 
     @Override
