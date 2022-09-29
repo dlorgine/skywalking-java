aa82c1e9a1cf42eb9babad21ab13635db3718e8e
lorgine-li <344649710@qq.com>
2022-09-29 20:53:42 +0800

    添加日志traceid

--- apm-sniffer/apm-sdk-plugin/cat-plugin/src/main/java/org/apache/skywalking/apm/plugin/cat/define/LogslfjInstrumentation.java
+++ apm-sniffer/apm-sdk-plugin/cat-plugin/src/main/java/org/apache/skywalking/apm/plugin/cat/define/LogslfjInstrumentation.java
@@ -4,13 +4,15 @@ import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.ConstructorInterceptPoint;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.InstanceMethodsInterceptPoint;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.StaticMethodsInterceptPoint;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.ClassInstanceMethodsEnhancePluginDefine;
+import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.ClassStaticMethodsEnhancePluginDefine;
 import org.apache.skywalking.apm.agent.core.plugin.match.ClassMatch;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.apache.skywalking.apm.agent.core.plugin.match.NameMatch.byName;
 
-public class LogslfjInstrumentation extends ClassInstanceMethodsEnhancePluginDefine {
+public class LogslfjInstrumentation extends ClassStaticMethodsEnhancePluginDefine {
 
     /**
      * Enhance class.
@@ -33,9 +35,9 @@ public class LogslfjInstrumentation extends ClassInstanceMethodsEnhancePluginDef
     }
 
     @Override
-    public InstanceMethodsInterceptPoint[] getInstanceMethodsInterceptPoints() {
-        return new InstanceMethodsInterceptPoint[] {
-                new InstanceMethodsInterceptPoint() {
+    public StaticMethodsInterceptPoint[] getStaticMethodsInterceptPoints() {
+        return new StaticMethodsInterceptPoint[] {
+                new StaticMethodsInterceptPoint() {
                     @Override
                     public ElementMatcher<MethodDescription> getMethodsMatcher() {
                         return named("getLogger");
